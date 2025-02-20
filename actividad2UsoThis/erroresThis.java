f4ec50f3e9c3277b50a9e0fe09aa78666771a69f
OsvaldoPachecoSanabria <osvaldo.pachecosanabria@unicolombo.edu.co>
2025-02-19 20:57:20 -0500

    Actividad2 #3 Errores de this evidenciado con un metodo static en este caso con un SOUT (this.nombre)

---
+++ actividad2UsoThis/erroresThis.java
@@ -0,0 +1,11 @@
+package actividad2UsoThis;
+
+public class erroresThis {
+
+    String nombre;
+
+    // ejemplo de error
+    public static void ejemploDeError() {
+        System.out.println(this.nombre);
+    }
+}
