1939d28f6848e614665f1a4f37496ad963f29be4
OsvaldoPachecoSanabria <osvaldo.pachecosanabria@unicolombo.edu.co>
2025-02-19 21:04:11 -0500

    Correccion Y ejemplo correcto De como usar this en un buen metodo static

--- actividad2UsoThis/erroresThis.java
+++ actividad2UsoThis/erroresThis.java
@@ -1,11 +1,24 @@
 package actividad2UsoThis;
 
 public class erroresThis {
-
+    // definicion de caracteristic
     String nombre;
 
-    // ejemplo de error
-    public static void ejemploDeError() {
+    public erroresThis(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void mostrarNombre() {
         System.out.println(this.nombre);
     }
+
+    public static void main(String[] args) {
+        erroresThis ejemplo = new erroresThis("Nombre generico");
+        ejemplo.mostrarNombre();
+    }
+    // ejemplo de error
+    // public static void ejemploDeError() {
+    // System.out.println(this.nombre);
+    // }
+
 }
