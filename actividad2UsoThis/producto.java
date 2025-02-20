fe75ec8131e1e730ded34f729a5c6feeb0ed2aed
OsvaldoPachecoSanabria <osvaldo.pachecosanabria@unicolombo.edu.co>
2025-02-19 19:10:19 -0500

    Actividad2 #1 Uso del this, Definicion clase y Constructor de la clase con parametros

---
+++ actividad2UsoThis/producto.java
@@ -0,0 +1,14 @@
+package actividad2UsoThis;
+
+public class producto {
+    // Definicion de clase prudcto atributos nombres y precio
+    String nombre;
+    double precio;
+
+    // Constructor de la clase producto con parametros nombre y precio
+    public producto(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+}
