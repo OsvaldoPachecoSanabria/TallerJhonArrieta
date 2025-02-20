0fef81e4146d7dca892c8aa8bcc31555fe3c78bd
OsvaldoPachecoSanabria <osvaldo.pachecosanabria@unicolombo.edu.co>
2025-02-19 21:17:39 -0500

    Actividad3 #1 Clase producto1 Creacion de class con las propiedades default, y constructor con acceso de paquete que inicie las propiedades

---
+++ actividad3/ClaseProducto1.java
@@ -0,0 +1,14 @@
+package actividad3;
+
+public class ClaseProducto1 {
+    String nombre;
+    double precio;
+    int stock;
+
+    // constructor que inicialice todas las propiedades
+    public ClaseProducto1(String nombre, double precio, int stock) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+}
