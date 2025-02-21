c46190872527e82eccc5df114335e4e289920276
OsvaldoPachecoSanabria <osvaldo.pachecosanabria@unicolombo.edu.co>
2025-02-20 22:07:50 -0500

    Ejercicio 2 taller 4, Clase coche con setters and getters junto al metodo acelerar Con validacion incluida

--- actividad4/ejercicio2/Coche.java
+++ actividad4/ejercicio2/Coche.java
@@ -1,5 +1,42 @@
 package actividad4.ejercicio2;
 
 public class Coche {
+    private String marca;
+    private String modelo;
+    private double velocidadMaxima;
 
+    // Getters and Setters
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public double getVelocidadMaxima() {
+        return velocidadMaxima;
+    }
+
+    public void setVelocidadMaxima(double velocidadMaxima) {
+        this.velocidadMaxima = velocidadMaxima;
+    }
+
+    // metodo para acelerar
+    public void acelerar(double incremento) {
+        if (incremento > 0) {
+            this.velocidadMaxima += incremento;
+        } else {
+            // si el incremento es negativo (validacion)
+            System.out.println("El incremento debe ser positivo");
+        }
+    }
 }
