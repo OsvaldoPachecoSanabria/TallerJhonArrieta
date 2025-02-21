aa5059ad84ae6cc158611940f59f29ef09ee0be4
OsvaldoPachecoSanabria <osvaldo.pachecosanabria@unicolombo.edu.co>
2025-02-20 23:22:24 -0500

    Clases y metodos que nos ayuden con las matematicas, ejemplo suma resta mult y div

---
+++ actividad7/ACTV2/Matematicas.java
@@ -0,0 +1,20 @@
+package actividad7.ACTV2;
+
+public class Matematicas {
+
+    public static int sumar(int a, int b) {
+        return a + b;
+    }
+
+    public static int restar(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    public static double dividir(int a, int b) {
+        return (double) a / b;
+    }
+}
---
+++ actividad7/ACTV2/PruebaMatematicas.java
@@ -0,0 +1,11 @@
+package actividad7.ACTV2;
+
+public class PruebaMatematicas {
+    public static void main(String[] args) {
+
+        System.out.println("Suma: " + Matematicas.sumar(5, 3));
+        System.out.println("Resta: " + Matematicas.restar(5, 3));
+        System.out.println("Multiplicación: " + Matematicas.multiplicar(5, 3));
+        System.out.println("División: " + Matematicas.dividir(5, 3)); // Esperado: 1.6666666666666667
+    }
+}
