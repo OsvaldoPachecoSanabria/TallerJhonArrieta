da59b38e818174d1775b72ab9340d4615cb82d2c
OsvaldoPachecoSanabria <osvaldo.pachecosanabria@unicolombo.edu.co>
2025-02-20 21:13:46 -0500

    Creacion de Prueba para colocar y Utilizar moverse()

---
+++ actividad3SobreEscritura/ejercicio2ClaseVYB/Prueba.java
@@ -0,0 +1,11 @@
+package actividad3SobreEscritura.ejercicio2ClaseVYB;
+
+public class Prueba {
+    public static void main(String[] args) {
+        Vehiculo vehiculo = new Bicicleta();
+        Vehiculo bicicleta = new Bicicleta();
+
+        vehiculo.moverse();
+        bicicleta.moverse();
+    }
+}
