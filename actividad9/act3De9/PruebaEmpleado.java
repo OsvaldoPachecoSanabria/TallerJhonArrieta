7dc555ab2cb3bac11b894c78360e1cc3480ef6d5
OsvaldoPachecoSanabria <osvaldo.pachecosanabria@unicolombo.edu.co>
2025-02-25 16:47:20 -0500

    Implementacion de del ruebaEmpleado con creacion de algunos empleados MostrarDetalles() de estos

--- actividad9/act3De9/PruebaEmpleado.java
+++ actividad9/act3De9/PruebaEmpleado.java
@@ -1,5 +1,13 @@
 package actividad9.act3De9;
 
 public class PruebaEmpleado {
+    public static void main(String[] args) {
+        Empleado empleado = new Empleado("Juan", 20000);
+        System.out.println("Empleado Creado: ");
+        empleado.mostrarDetalles();
 
+        Gerente gerente = new Gerente("Maria", 30000, "Sistemas");
+        System.out.println("Gerente Creado: ");
+        gerente.mostrarDetalles();
+    }
 }
