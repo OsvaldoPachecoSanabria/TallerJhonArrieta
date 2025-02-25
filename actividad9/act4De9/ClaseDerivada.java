7ac29ec8fc8bb1bbb729db56081ffbf264446e2f
OsvaldoPachecoSanabria <osvaldo.pachecosanabria@unicolombo.edu.co>
2025-02-25 17:23:06 -0500

    Corrreccion del error

--- actividad9/act4De9/ClaseDerivada.java
+++ actividad9/act4De9/ClaseDerivada.java
@@ -1,10 +1,12 @@
 package actividad9.act4De9;
 
-public class ClaseDerivada {
+public class ClaseDerivada extends ClaseBase {
     public void mostrarAtributoPrivado() {
 
         // Error: intento De acceso Directo al atributo Privado(esto causará un error de
         // compilación)
         // System.out.println("ClaseDerivada.metodoDerivado" + atributoPrivado);
+
+        System.out.println("Atributo Privado: " + getAtributoPrivado());
     }
 }
