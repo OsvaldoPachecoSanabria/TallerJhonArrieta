c71fbb5ef6736febef23b0b5227d04f3bf4fae34
OsvaldoPachecoSanabria <osvaldo.pachecosanabria@unicolombo.edu.co>
2025-02-25 17:53:12 -0500

    Ultima PRueba de Clase Derivada porque los atributos privados de una clase base no son accesibles directamente desde las clases derivadas

---
+++ actividad10/actv10Tres/Prueba.java
@@ -0,0 +1,8 @@
+package actividad10.actv10Tres;
+
+public class Prueba {
+    public static void main(String[] args) {
+        ClaseDerivada derivada = new ClaseDerivada();
+        derivada.mostrarAtributoPrivado();
+    }
+}
