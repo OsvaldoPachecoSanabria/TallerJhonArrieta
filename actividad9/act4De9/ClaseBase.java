06547a8448a184a60c12d03ceda44ddca4389898
OsvaldoPachecoSanabria <osvaldo.pachecosanabria@unicolombo.edu.co>
2025-02-25 17:18:34 -0500

    intento De acceso Directo al atributo. Esto con 2 Clases base Y Derivadas.

---
+++ actividad9/act4De9/ClaseBase.java
@@ -0,0 +1,9 @@
+package actividad9.act4De9;
+
+public class ClaseBase {
+    private String atributoPrivado = "Valor Privado (oculto)";
+
+    public String getAtributoPrivado() {
+        return atributoPrivado;
+    }
+}
---
+++ actividad9/act4De9/ClaseDerivada.java
@@ -0,0 +1,10 @@
+package actividad9.act4De9;
+
+public class ClaseDerivada {
+    public void mostrarAtributoPrivado() {
+
+        // Error: intento De acceso Directo al atributo Privado(esto causará un error de
+        // compilación)
+        // System.out.println("ClaseDerivada.metodoDerivado" + atributoPrivado);
+    }
+}
